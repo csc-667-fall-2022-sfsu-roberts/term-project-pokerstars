90e6a38f59f616c16b69d8a3ef5cac4fe29ebe96
antnewyen2512 <antnewyen2512@gmail.com>
2022-10-13 22:19:52 -0700

    Initial Commite

---
+++ routes/index.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
+const pgtools = require("pgtools");
+const config = {
+  user: "pokestars_user",
+  host: "dpg-cd4bjipgp3jqpbo8m10g-a",
+  password: "mqlcmKyZXDE4R41MA322YIfdqZLKlf7H",
+  port: 5432
+};
+
+pgtools.createdb(config, "pokerstars", function(err, res) {
+  if (err) {
+    console.error(err);
+    process.exit(-1);
+  }
+  console.log(res);
+});
