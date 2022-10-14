9c7bfad4f6b205e15cc94c96dbe55e8abea74c3f
antnewyen2512 <antnewyen2512@gmail.com>
2022-10-14 12:21:05 -0700

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
+var testRouter = require('./routes/test');
 var app = express();
 
 // view engine setup
@@ -24,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/test', testRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
