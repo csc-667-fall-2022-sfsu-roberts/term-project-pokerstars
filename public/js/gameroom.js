ea2734428e68f1f7bdad90215c28202955e71ae7
Cris Padilla <crispadilla82@gmail.com>
2022-12-05 20:17:47 -0800

    Fixed error in public/js/cards.js where jquery import was redundant

--- public/js/gameroom.js
+++ public/js/gameroom.js
@@ -1,8 +1,8 @@
-$(document).ready(function () {
-  $.getScript("cards.js", function () {
-    getCardImage();
-  });
-});
+// $(document).ready(function () {
+//   $.getScript("cards.js", function () {
+//     getCardImage();
+//   });
+// });
 
 let card1 = document.getElementById("player1");
 let commonCard_1 = document.getElementById("cc1");
@@ -11,7 +11,7 @@ let commonCard_3 = document.getElementById("cc3");
 let commonCard_4 = document.getElementById("cc4");
 let commonCard_5 = document.getElementById("cc5");
 
-card1.children[0].classList.add(getCardImage(5));
+card1.children[0].classList.add(cardImages.get(5));
 card1.children[1].classList.add(cardImages.get(35));
 commonCard_1.classList.add(cardImages.get(15));
 commonCard_2.classList.add(cardImages.get(20));
